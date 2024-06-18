1ff34bc7c9f277bf48dd812a7c2e3c567c17595a
MJ Schanne <mjschanne@microsoft.com>
2024-06-18 09:34:31 -0400

    Add singleton Kernel to dependency injection with plugins loaded

--- src/Orchestrator/Program.cs
+++ src/Orchestrator/Program.cs
@@ -1,14 +1,37 @@
+using Azure.Identity;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.SemanticKernel;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+    .ConfigureServices((hostContext, services) =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+
+        var modelId = hostContext.Configuration["AOAI_MODEL_ID"] ?? throw new Exception("AOAI_MODEL_ID missing in configuration.");
+        var endpoint = hostContext.Configuration["AOAI_ENDPOINT"] ?? throw new Exception("AOAI_ENDPOINT missing in configuration.");
+        var pluginDir = hostContext.Configuration["AOAI_PLUGIN_DIR"] ?? throw new Exception("AOAI_PLUGIN_DIR missing in configuration.");
+        var apiKey = hostContext.Configuration["AOAI_API_KEY"];
+
+        var credential = new DefaultAzureCredential();
+
+        var builder = string.IsNullOrWhiteSpace(apiKey)
+            ? Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, credential)
+            : Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+
+        var plugins = Directory.GetDirectories(pluginDir, "*", SearchOption.TopDirectoryOnly);
+
+        foreach (var plugin in plugins)
+            builder.Plugins.AddFromPromptDirectory(plugin);
+
+        var kernel = builder.Build();
+
+        services.AddSingleton(kernel);
     })
     .Build();
 
-host.Run();
+await host.RunAsync();
