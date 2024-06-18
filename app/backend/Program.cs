31ede120606d092471425266794327446c6279bd
Phil Jirsa <phil.jirsa@microsoft.com>
2024-06-18 07:22:37 -0500

    update nugets, remove anti-forgery

--- app/backend/Program.cs
+++ app/backend/Program.cs
@@ -1,7 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using Microsoft.AspNetCore.Antiforgery;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.ConfigureAzureKeyVault();
@@ -14,7 +12,6 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddCrossOriginResourceSharing();
 builder.Services.AddAzureServices();
-builder.Services.AddAntiforgery(options => { options.HeaderName = "X-CSRF-TOKEN-HEADER"; options.FormFieldName = "X-CSRF-TOKEN-FORM"; });
 builder.Services.AddHttpClient();
 
 if (builder.Environment.IsDevelopment())
@@ -84,17 +81,9 @@ app.UseRouting();
 app.UseStaticFiles();
 app.UseCors();
 app.UseBlazorFrameworkFiles();
-app.UseAntiforgery();
 app.MapRazorPages();
 app.MapControllers();
 
-app.Use(next => context =>
-{
-    var antiforgery = app.Services.GetRequiredService<IAntiforgery>();
-    var tokens = antiforgery.GetAndStoreTokens(context);
-    context.Response.Cookies.Append("XSRF-TOKEN", tokens?.RequestToken ?? string.Empty, new CookieOptions() { HttpOnly = false });
-    return next(context);
-});
 app.MapFallbackToFile("index.html");
 
 app.MapApi();
