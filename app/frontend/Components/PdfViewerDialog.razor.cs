681b4c251f300053f34ed5512879887af2d257f6
MJ Schanne <mjschanne@microsoft.com>
2024-06-19 12:39:39 -0400

    Add buttons for sharing to email and teams

--- app/frontend/Components/PdfViewerDialog.razor.cs
+++ app/frontend/Components/PdfViewerDialog.razor.cs
@@ -1,9 +1,19 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+using System.Security.Policy;
+using Microsoft.JSInterop;
+
 namespace ClientApp.Components;
 
 public sealed partial class PdfViewerDialog
 {
+    private readonly IJSRuntime _runtime;
+
+    public PdfViewerDialog(IJSRuntime jsRuntime)
+    {
+        _runtime = jsRuntime;
+    }
+
     private bool _isLoading = true;
     private string _pdfViewerVisibilityStyle => _isLoading ? "display:none;" : "display:default;";
 
@@ -22,6 +32,18 @@ public sealed partial class PdfViewerDialog
                 _isLoading = false;
                 StateHasChanged();
             });
+    }
+
+
+    private async Task SendToEmailAsync()
+    {
+        // does filename need to be encoded?
+        await _runtime.InvokeVoidAsync("open", "navigate", $"mailto:?subject={FileName}&amp;body=Check out this document {BaseUrl}.");
+    }
+
+    private async Task SendToTeamsAsync()
+    {
+       await _runtime.InvokeVoidAsync("sendToTeams", FileName, BaseUrl);
     }
 
     private void OnCloseClick() => Dialog.Close(DialogResult.Ok(true));
