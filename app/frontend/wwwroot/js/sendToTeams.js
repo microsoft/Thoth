7aea2d70ebee35403f3dceef65861af627c78ad7
MJ Schanne <mjschanne@microsoft.com>
2024-06-19 12:38:33 -0400

    Add TeamsJS Library

---
+++ app/frontend/wwwroot/js/sendToTeams.js
@@ -0,0 +1,12 @@
+﻿window.sendToTeams = (filename, url) => {
+    microsoftTeams.sharing.shareWebContent({
+        content: [
+            {
+                type: 'URL',
+                url: url,
+                message: `Check out this document I found using Microsoft Thoth: ${filename}.`,
+                preview: true
+            }
+        ]
+    });
+}
